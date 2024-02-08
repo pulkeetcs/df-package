40871ef0759b085fe64c9475ae87b0ade7e0b54f
Pulkeet Gupta <pulkeet.gupta@cloudsufi.com>
2024-02-08 08:13:29 +0000

    index file moified

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-// TODO: import files from includes
-// const file_one = require("./includes/file_one");
-// const file_two = require("./includes/file_two");
+// filename index.js
+// package name postoffice
 
-module.exports = (params) => {
+const GENERIC_DOMAINS = "('samplemail.com','samplemail.co.uk','examplemailbox.com'";
 
-    params = {
-      // TODO: set default params
-
-      ...params
-    };
-
-    // Publish and return datasets.
-    let result = {
-    // TODO: update files to call with params
-    //   file_one: file_one(params),
-    //   file_two: file_two(params)
-    };
-
-    return result;
+function getDomain(email) {
+  let cleanEmail = `trim(${email})`
+  const domain = `substr(${cleanEmail}, strpos(${cleanEmail}, '@') + 1)`;
+  return `case
+            when ${domain} in ${common.GENERIC_DOMAINS} then ${cleanEmail}
+            when ${domain} = "othermailbox.com" then "other.com"
+            when ${domain} = "mailbox.com" then "mailbox.global"
+            when ${domain} = "support.postman.com" then "postman.com"
+            else ${domain}
+          end`;
 }
+
+module.exports = { getDomain }
